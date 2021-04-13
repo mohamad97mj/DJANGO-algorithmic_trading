1525946e550ff2ed25fc013c1e44f4650ae12533
mohamad <mohamad97mj@gmail.com>
2021-04-13 22:44:59 +0430

    primary apis tested successfully

---
+++ binance_bot/celery.py
@@ -0,0 +1,26 @@
+from trader.tasks import get_open_orders
+from celery import Celery
+from celery.schedules import crontab
+
+app = Celery()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    # sender.add_periodic_task(10.0, get_all_orders.s(), name='get every 10')
+    sender.add_periodic_task(10.0, get_open_orders.s(), name='get every 10')
+
+    # Calls test('world') every 30 seconds
+    # sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    # sender.add_periodic_task(
+    #     crontab(hour=7, minute=30, day_of_week=1),
+    #     test.s('Happy Mondays!'),
+    # )
+
+
+@app.task
+def test(arg):
+    print(arg)
--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 import os
-
+from pprint import pprint
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 from twisted.internet import reactor
 
-api_key = 'knBxiPJqJhGwpADVP4sqvH8RXLnc8vBPIB1iTKDONQ07q2VD9Ik7KCmqxCw1rrMT'  # test
-# api_key = 'm6IcuuOFD4r1OiS5Mw87UjNjiKPav14CviOOyxz6CvG17AY1QBIxuPF7v6scDuqg' #main
-api_secret = '4Pf1cmFJ7qi5aefurLjQSIvb3iGCDcEnkgPi4qvlDSulcFQlbQZgIGTgHcUJgvDG'  # test
-# api_secret = 'B0CdJ9l95FOdtsZPdZ4oNRhnqj2mor7EniB99icmrxUW8bt5eoRZhk8CVCRfQau5' $main
+# api_key = 'knBxiPJqJhGwpADVP4sqvH8RXLnc8vBPIB1iTKDONQ07q2VD9Ik7KCmqxCw1rrMT'  # test
+api_key = 'm6IcuuOFD4r1OiS5Mw87UjNjiKPav14CviOOyxz6CvG17AY1QBIxuPF7v6scDuqg' #main
+# api_secret = '4Pf1cmFJ7qi5aefurLjQSIvb3iGCDcEnkgPi4qvlDSulcFQlbQZgIGTgHcUJgvDG'  # test
+api_secret = 'B0CdJ9l95FOdtsZPdZ4oNRhnqj2mor7EniB99icmrxUW8bt5eoRZhk8CVCRfQau5' #main
 client = Client(api_key, api_secret)
-client.API_URL = 'https://testnet.binance.vision/api'
+# client.API_URL = 'https://testnet.binance.vision/api'
 
-# print(client.get_account())
+pprint(client.get_account())
 
-print(client.get_asset_balance(asset='BTC'))
+pprint(client.get_asset_balance(asset='BTC'))
 
 # get latest price from Binance API
 btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
