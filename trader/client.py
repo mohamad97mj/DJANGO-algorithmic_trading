2bdfa8ec662e16b709639e7744eb145ba7b50d72
Author Name <mohamad97mj@gmail.com>
2021-05-13 00:58:48 +0430

    api_keys moved to enviroment variables

--- trader/client.py
+++ trader/client.py
@@ -1,14 +1,14 @@
 from binance.client import Client
+import os
 
-api_key = 'knBxiPJqJhGwpADVP4sqvH8RXLnc8vBPIB1iTKDONQ07q2VD9Ik7KCmqxCw1rrMT'  # test
-# api_key = 'm6IcuuOFD4r1OiS5Mw87UjNjiKPav14CviOOyxz6CvG17AY1QBIxuPF7v6scDuqg' #main
-api_secret = '4Pf1cmFJ7qi5aefurLjQSIvb3iGCDcEnkgPi4qvlDSulcFQlbQZgIGTgHcUJgvDG'  # test
-# api_secret = 'B0CdJ9l95FOdtsZPdZ4oNRhnqj2mor7EniB99icmrxUW8bt5eoRZhk8CVCRfQau5' #main
+api_key = os.environ.get('TEST_API_KEY')  # test
+api_secret = os.environ.get('TEST_SECRET_KEY')  # test
 proxies = {
     'http': 'http://ck2.slike.vip:2087',
     'https': 'http://ck2.slike.vip:2087'
 }
-client = Client(api_key, api_secret,
+
+client = Client(api_key, api_secret, testnet=True
                 # {'proxies': proxies}
                 )
-client.API_URL = 'https://testnet.binance.vision/api'
+# client.API_URL = 'https://testnet.binance.vision/api'
--- trader/main.py
+++ trader/main.py
@@ -3,7 +3,7 @@ from trader.spot_bot import SpotBotClient
 
 def main():
     spot_bot = SpotBotClient()
-    # spot_bot.get_exchange_info()
+    spot_bot.get_exchange_info()
     spot_bot.get_symbol_all_ordres()
 
 
