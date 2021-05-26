e8be94b17a888b46dcdd1e749adedc3312da2e54
Author Name <mohamad97mj@gmail.com>
2021-05-26 10:23:49 +0430

    junks were removed in main

--- trader/main.py
+++ trader/main.py
@@ -3,28 +3,6 @@ from trader.clients import GeneralClient, PublicClient, SpotClient
 
 def main():
 
-    # market_data_client = PublicClient()
-    # market_data_client.get_recent_trades()
-    # market_data_client.get24hr_ticker()
-    # market_data_client.get_symbol_avg_price(symbol='ETHUSDT')
-    # market_data_client.get_all_prices()
-    # market_data_client.get_candles()
-
-    # spot_client = SpotClient()
-    # spot_client.get_dayli_account_snapshot()
-    # spot_client.get_account_info()
-    # spot_client.place_buy_market_order(symbol='ETHUSDT', quantity=1)
-    # spot_client.get_symbol_all_trades(symbol='ETHUSDT')
-    # spot_client.place_buy_market_order(0.75, symbol='ETHUSDT')
-    # spot_client.place_sell_market_order(0.75, symbol='ETHUSDT')
-    # spot_client.place_buy_limit_order(1.5, 150, symbol='ETHUSDT')
-    # spot_client.place_sell_limit_order(1.25, 210, symbol='ETHUSDT')
-    # spot_client.get_symbol_all_orders('ETHUSDT')
-    # spot_client.get_symbol_all_open_orders('ETHUSDT')
-    # spot_client.get_symbol_all_orders('ETHUSDT')
-    # spot_client.get_trade_fees()
-    # spot_client.get_symbol_trade_fees()
-
 
 if __name__ == "__main__":
     main()
