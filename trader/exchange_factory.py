e6514ddc6c90cf174b27abe4333ac7a63ad3e516
Author Name <mohamad97mj@gmail.com>
2021-05-26 11:26:25 +0430

    enable_rate_limit and requests time out was implemented in exchange factory

--- trader/exchange_factory.py
+++ trader/exchange_factory.py
@@ -4,12 +4,16 @@ from .auth import credentials
 
 class ExchageFactory:
     @staticmethod
-    def create_exchange(exchange_id='binance', credential_id='test', sandbox_mode=True):
+    def create_exchange(exchange_id='binance',
+                        credential_id='test',
+                        enable_rate_limit=True,
+                        timeout=3000,
+                        sandbox_mode=True):
         exchange = getattr(ccxt, exchange_id)({
             'apiKey': credentials[credential_id]['api_key'],
             'secret': credentials[credential_id]['secret_key'],
-            'timeout': 30000,
-            'enableRateLimit': True,
+            'timeout': timeout,
+            'enableRateLimit': enable_rate_limit,
         })
         exchange.set_sandbox_mode(sandbox_mode)
         return exchange
