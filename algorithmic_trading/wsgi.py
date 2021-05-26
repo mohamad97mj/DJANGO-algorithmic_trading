8917568708cbaab8a623022acdbde8f318ee8f58
Author Name <mohamad97mj@gmail.com>
2021-05-27 00:00:08 +0430

    app and project was renamed

--- algorithmic_trading/wsgi.py
+++ algorithmic_trading/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for binance_bot project.
+WSGI config for algorithmic_trading project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'binance_bot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'algorithmic_trading.settings')
 
 application = get_wsgi_application()
