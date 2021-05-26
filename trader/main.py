841e5f8b0f46794a431e48d72d8c073a5a3442fd
Author Name <mohamad97mj@gmail.com>
2021-05-26 12:29:35 +0430

    get_exchanges method in public client was tested

--- trader/main.py
+++ trader/main.py
@@ -1,7 +1,12 @@
 from trader.bots import SpotTrader
+from trader.clients import PublicClient
 
 
 def main():
+    # public_client = PublicClient()
+    # print('exchanges are:', public_client.get_exchanges())
+
+
     pass
 
 
