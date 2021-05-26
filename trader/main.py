e92c93a748e9a33f9cbeb500f4c7a3c9ed42b237
mohamad <mohamad97mj@gmail.com>
2021-05-26 23:00:57 +0430

    SpotPosition class was craeted

--- trader/main.py
+++ trader/main.py
@@ -8,10 +8,11 @@ from pprint import pprint
 async def main():
     ef = ExchageFactory()
     exchange = ef.create_exchange(exchange_id='binance')
-    # public_client = PublicClient(exchange=exchange)
-    # pprint(await public_client.fetch_ohlcv(limit=5))
-    # await ef.close_all_exchages()
-    pprint(exchange.get_timeframes())
+    public_client = PublicClient(exchange=exchange)
+    x = await public_client.fetch_ohlcv(timeframe='1m')
+    pprint(x)
+    await ef.close_all_exchages()
+    # pprint(exchange.get_timeframes())
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
