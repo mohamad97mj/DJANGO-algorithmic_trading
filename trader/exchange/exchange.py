3a430a8ef568136d9569dcf3e102bec40da9f263
mohamad <mohamad97mj@gmail.com>
2021-05-26 12:26:27 +0430

    get_exchanges was implemented in PublicCient

--- trader/exchange/exchange.py
+++ trader/exchange/exchange.py
@@ -1,3 +1,8 @@
+import ccxt.async_support as ccxt
+
+
 class Exchange:
-    def __init__(self, third_party_exchange):
+    def __init__(self, third_party_exchange: ccxt.Exchange):
         self._third_party_exchange = third_party_exchange
+
+
