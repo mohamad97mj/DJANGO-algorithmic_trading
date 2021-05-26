3a03a2229b0a3bceac02435c679717651cb48b93
mohamad <mohamad97mj@gmail.com>
2021-05-26 11:09:21 +0430

    credential was added to exchange initialization

--- trader/exchange_factory.py
+++ trader/exchange_factory.py
@@ -1,10 +1,14 @@
 import ccxt.async_support as ccxt
+from .auth import credentials
 
 
 class ExchageFactory:
     @staticmethod
-    def create_exchange(id='binance'):
+    def create_exchange(id='binance', credential='test'):
         exchange = getattr(ccxt, id)({
+            'apiKey': credentials[credential]['api_key'],
+            'secret': credentials[credential]['secret_key'],
+            'timeout': 30000,
             'enableRateLimit': True,
         })
         return exchange
