fc14429129a4088fcf9bb447f71ae5575917732d
Author Name <mohamad97mj@gmail.com>
2021-05-26 11:46:15 +0430

    Exchange was created

--- trader/exchange/exchange_factory.py
+++ trader/exchange/exchange_factory.py
@@ -1,5 +1,6 @@
 import ccxt.async_support as ccxt
-from .auth import credentials
+from trader.auth import credentials
+from .exchange import Exchange
 
 
 class ExchageFactory:
@@ -7,13 +8,11 @@ class ExchageFactory:
     def create_exchange(exchange_id='binance',
                         credential_id='test',
                         enable_rate_limit=True,
-                        timeout=3000,
                         sandbox_mode=True):
         exchange = getattr(ccxt, exchange_id)({
             'apiKey': credentials[credential_id]['api_key'],
             'secret': credentials[credential_id]['secret_key'],
-            'timeout': timeout,
             'enableRateLimit': enable_rate_limit,
         })
         exchange.set_sandbox_mode(sandbox_mode)
-        return exchange
+        return Exchange()
