191c7e5a9c6907687b0a25ebe5cd8a881315cdf8
Author Name <mohamad97mj@gmail.com>
2021-05-26 11:24:16 +0430

    sandbox mode was implemented

--- trader/exchange_factory.py
+++ trader/exchange_factory.py
@@ -4,11 +4,12 @@ from .auth import credentials
 
 class ExchageFactory:
     @staticmethod
-    def create_exchange(id='binance', credential='test'):
-        exchange = getattr(ccxt, id)({
-            'apiKey': credentials[credential]['api_key'],
-            'secret': credentials[credential]['secret_key'],
+    def create_exchange(exchange_id='binance', credential_id='test', sandbox_mode=True):
+        exchange = getattr(ccxt, exchange_id)({
+            'apiKey': credentials[credential_id]['api_key'],
+            'secret': credentials[credential_id]['secret_key'],
             'timeout': 30000,
             'enableRateLimit': True,
         })
+        exchange.set_sandbox_mode(sandbox_mode)
         return exchange
