b7d86195db6472b5a1dd03a8f9a8712b4d0bef07
mohamad <mohamad97mj@gmail.com>
2021-05-26 17:46:38 +0430

    method fetch_order_book was implemented

--- trader/clients/public_client.py
+++ trader/clients/public_client.py
@@ -13,11 +13,15 @@ class PublicClient:
         return ccxt.exchanges
 
     async def load_markets(self, reload=False):
-        markets = await self._exchange.load_markets(reload)
-        return markets
+        return await self._exchange.load_markets(reload=reload)
 
     async def get_markets(self):
         return await self._exchange.get_markets()
 
     async def get_market(self, symbol='BTC/USDT'):
-        return await self._exchange.get_market(symbol)
+        return await self._exchange.get_market(symbol=symbol)
+
+    async def fetch_order_book(self, symbol='BTC/USDT'):
+        return await self._exchange.fetch_order_book(symbol=symbol)
+
+
