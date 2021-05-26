e1de4b8ba33a8bf70dc75f31c3828b6faec9c65b
mohamad <mohamad97mj@gmail.com>
2021-05-26 12:37:32 +0430

    load_markets method in exchange was implemented

--- trader/exchange/exchange.py
+++ trader/exchange/exchange.py
@@ -5,4 +5,6 @@ class Exchange:
     def __init__(self, third_party_exchange: ccxt.Exchange):
         self._third_party_exchange = third_party_exchange
 
-
+    def load_markets(self):
+        markets = self._third_party_exchange.load_markets()
+        print(self._third_party_exchange.id, markets)
