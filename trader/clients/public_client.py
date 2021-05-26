d07de2c000114896cac0e8748faa051d73f224fe
Author Name <mohamad97mj@gmail.com>
2021-05-26 10:23:16 +0430

    Clients are public and private now

--- trader/clients/public_client.py
+++ trader/clients/public_client.py
@@ -6,6 +6,8 @@ from binance.client import Client
 
 @apply2all_methods(log)
 class PublicClient:
+    def __init__(self):
+        self._client = client
 
     # def ping_server(self):
     #     res = self._client.ping()
@@ -35,32 +37,32 @@ class PublicClient:
     #     pprint(info)
     #     return info
 
-    def get_all_exchanges(self):
-        exchanges = ccxt.exchanges
-        pprint(exchanges)
-        return exchanges
-
-    def get_recent_trades(self, symbol='BTCUSDT'):
-        trades = client.get_recent_trades(symbol=symbol)
-        pprint(trades)
-        return trades
-
-    def get_symbol_avg_price(self, symbol='BTCUSDT'):
-        avg_price = client.get_avg_price(symbol=symbol)
-        pprint(avg_price)
-        return avg_price
-
-    def get24hr_ticker(self):
-        tickers = client.get_ticker()
-        pprint(tickers)
-        return tickers
-
-    def get_all_prices(self):
-        info = client.get_all_tickers()
-        pprint(info)
-        return info
-
-    def get_candles(self, symbol='BTCUSDT'):
-        candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_30MINUTE)
-        pprint(candles)
-        return candles
+    # def get_all_exchanges(self):
+    #     exchanges = ccxt.exchanges
+    #     pprint(exchanges)
+    #     return exchanges
+    #
+    # def get_recent_trades(self, symbol='BTCUSDT'):
+    #     trades = client.get_recent_trades(symbol=symbol)
+    #     pprint(trades)
+    #     return trades
+    #
+    # def get_symbol_avg_price(self, symbol='BTCUSDT'):
+    #     avg_price = client.get_avg_price(symbol=symbol)
+    #     pprint(avg_price)
+    #     return avg_price
+    #
+    # def get24hr_ticker(self):
+    #     tickers = client.get_ticker()
+    #     pprint(tickers)
+    #     return tickers
+    #
+    # def get_all_prices(self):
+    #     info = client.get_all_tickers()
+    #     pprint(info)
+    #     return info
+    #
+    # def get_candles(self, symbol='BTCUSDT'):
+    #     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_30MINUTE)
+    #     pprint(candles)
+    #     return candles
