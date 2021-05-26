c72cc171be581571b373fdc41831b2b075cb6823
mohamad <mohamad97mj@gmail.com>
2021-05-26 23:46:29 +0430

    project was restructued in case of spotbot models and serializer

---
+++ trader/bots/spot/models/strategy.py
@@ -0,0 +1,4 @@
+
+
+class SpotStrategy:
+    pass
--- trader/bots/spot/spot_position.py
+++
@@ -1,5 +0,0 @@
-class SpotPosition:
-    def __init__(self, signal, volume, strategy):
-        self.signal = signal
-        self.volume = volume
-        self.strategy = strategy
