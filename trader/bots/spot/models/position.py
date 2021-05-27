3822dafe18f49efcd96a13b37880cf1f5bf33267
Author Name <mohamad97mj@gmail.com>
2021-05-27 13:40:01 +0430

    strategy model was created

--- trader/bots/spot/models/position.py
+++ trader/bots/spot/models/position.py
@@ -4,4 +4,4 @@ from django.db import models
 class SpotPosition(models.Model):
     signal = models.ForeignKey('SpotSignal', on_delete=models.RESTRICT)
     volume = models.FloatField()
-    strategy = models.ForeignKey('SpotStrategy', on_delete=models.RESTRICT)
+    strategy = models.ForeignKey('SpotStrategy', related_name='position', on_delete=models.RESTRICT)
