38a1a0ab394a6632b8f3c8e6112b477a6d1fff43
Author Name <mohamad97mj@gmail.com>
2021-05-27 23:53:23 +0430

    exchange initialization was moved into clients

--- trader/bots/spot/models/bot.py
+++ trader/bots/spot/models/bot.py
@@ -5,12 +5,13 @@ from .strategy import SpotStrategy
 from .operation import Operation
 from trader.bots.spot.strategy_center import SpotStrategyCenter
 from .exceptions import BotDoesNotExistsException
+from trader.clients.private_client import PrivateClient
 
 
 class SpotBotManager(models.Manager):
-    def _find_from_db(self, id):
+    def find_from_db(self, bot_id):
         try:
-            bot = self.get(id=id)
+            bot = self.get(bot_id=bot_id)
             return bot
         except models.ObjectDoesNotExist:
             raise BotDoesNotExistsException()
@@ -20,20 +21,20 @@ class SpotBot(models.Model):
     objects = SpotBotManager()
 
     bot_id = models.CharField(max_length=100, unique=True)
-    credential = models.CharField(max_length=50)
+    credential_id = models.CharField(max_length=50)
     current_strategy_id = models.BigIntegerField()
 
-    def __init__(self, exchange: Exchange, position: SpotPosition, strategy_name: str, *args, **kwargs):
-        super(SpotBot, self).__init__(*args, **kwargs)
-        self._exchange = exchange
+    def __init__(self, credential_id: str, position: SpotPosition, strategy_name: str, *args, **kwargs):
+        super(SpotBot, self).__init__(credential_id=credential_id, *args, **kwargs)
+        self._private_client = PrivateClient(credential_id)
         self._current_strategy = None
         self.set_strategy(strategy_name, position)
 
     def set_strategy(self, strategy_name, position):
-        self.current_strategy = SpotStrategyCenter.get_strategy(strategy_name, position)
-        self.current_strategy.bot = self
-        self.current_strategy.save()
-        self.current_strategy_id = self.current_strategy.id
+        self._current_strategy = SpotStrategyCenter.get_strategy(strategy_name, position)
+        self._current_strategy.bot = self
+        self._current_strategy.save()
+        self.current_strategy_id = self._current_strategy.id
         self.save()
 
     def reload_bot(self):
@@ -41,10 +42,10 @@ class SpotBot(models.Model):
         self._reload_operations()
 
     def _reload_strategy(self):
-        self.current_strategy: SpotStrategy = self.strategies.get(id=self.current_strategy_id)
+        self._current_strategy: SpotStrategy = self.strategies.get(id=self.current_strategy_id)
 
     def _reload_operations(self):
-        self._operations: SpotOperation = self.current_strategy.position.operations.all()
+        self._operations: SpotOperation = self._current_strategy.position.operations.all()
 
     def run(self):
         pass
