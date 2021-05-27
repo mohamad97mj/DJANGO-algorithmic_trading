ec69f96c25424b96d60607af3d01707826ac2256
mohamad <mohamad97mj@gmail.com>
2021-05-27 12:48:50 +0430

    order model was created

---
+++ trader/bots/spot/models/order.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class SpotOrder:
+    id = models.CharField(max_length=100)
+    datetime = models.DateTimeField()
+    timestamp = models.BigIntegerField()
+    status = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    side = models.CharField(max_length=50)
+    price = models.FloatField()
+    average = models.FloatField()
+    amount = models.FloatField()
+    filled = models.FloatField()
+    remaining = models.FloatField()
+    cost = models.FloatField()
+    fee = models.JSONField()
