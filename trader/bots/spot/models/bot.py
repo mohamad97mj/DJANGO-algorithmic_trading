8292f3990c7c0c0b012e0e5103812ea9cc312578
Author Name <mohamad97mj@gmail.com>
2021-05-27 23:07:57 +0430

    SpotBot reload_bot and its submethods was implemented

--- trader/bots/spot/models/bot.py
+++ trader/bots/spot/models/bot.py
@@ -1,6 +1,8 @@
 from django.db import models
 from trader.exchange import Exchange
-from trader.bots.spot.models import SpotPosition
+from .position import SpotPosition
+from .strategy import SpotStrategy
+from .operation import Operation
 from trader.bots.spot.strategy_center import SpotStrategyCenter
 from .exceptions import BotDoesNotExistsException
 
@@ -15,20 +17,20 @@ class SpotBotManager(models.Manager):
 
 
 class SpotBot(models.Model):
-    bot_id = models.CharField(max_length=100, unique=True)
     objects = SpotBotManager()
+
+    bot_id = models.CharField(max_length=100, unique=True)
     credential = models.CharField(max_length=50)
     current_strategy_id = models.BigIntegerField()
 
     def __init__(self, exchange: Exchange, position: SpotPosition, strategy_name: str, *args, **kwargs):
         super(SpotBot, self).__init__(*args, **kwargs)
         self._exchange = exchange
-        self._position = position
-        self.current_strategy = None
-        self.set_strategy(strategy_name)
+        self._current_strategy = None
+        self.set_strategy(strategy_name, position)
 
-    def set_strategy(self, strategy_name):
-        self.current_strategy = SpotStrategyCenter.get_strategy(strategy_name, self._position)
+    def set_strategy(self, strategy_name, position):
+        self.current_strategy = SpotStrategyCenter.get_strategy(strategy_name, position)
         self.current_strategy.bot = self
         self.current_strategy.save()
         self.current_strategy_id = self.current_strategy.id
@@ -36,10 +38,13 @@ class SpotBot(models.Model):
 
     def reload_bot(self):
         self._reload_strategy()
+        self._reload_operations()
 
     def _reload_strategy(self):
-        self._strategy = self.strategies.get(id=self.current_strategy_id)
+        self.current_strategy: SpotStrategy = self.strategies.get(id=self.current_strategy_id)
+
+    def _reload_operations(self):
+        self._operations: SpotOperation = self.current_strategy.position.operations.all()
 
     def run(self):
-        # operations = self.current_strategy
         pass
