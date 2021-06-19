7476f04e3ac7994fca200e71e3055c64ddbcea78
mohamad <mohamad97mj@gmail.com>
2021-06-19 19:55:31 +0430

    create method was implemented in seriposition serializer and tiem zone was added to settings

--- trader/bots/spot/serializers/position.py
+++ trader/bots/spot/serializers/position.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .signal import SpotSignalSerializer
 from .strategy import SpotStrategySerializer
+from trader.bots.spot.models import SpotPosition, SpotSignal, SpotStrategy
 
 
 class SpotPositionSerializer(serializers.Serializer):
@@ -8,3 +9,11 @@ class SpotPositionSerializer(serializers.Serializer):
     signal = SpotSignalSerializer()
     volume = serializers.FloatField()
     strategy = SpotStrategySerializer()
+
+    def create(self, validated_data):
+        signal = SpotSignal(**validated_data['signal'])
+        strategy = SpotStrategy(**validated_data['strategy'])
+        return SpotPosition(position_id=validated_data['position_id'],
+                            signal=signal,
+                            volume=validated_data['volume'],
+                            strategy=strategy)
--- trader/main.py
+++ trader/main.py
@@ -6,7 +6,7 @@ from pprint import pprint
 
 
 async def main():
-    public_client = PublicClient(exchange_id='binance')
+    # public_client = PublicClient(exchange_id='binance')
     # private_client = PrivateClient(exchange_id='binance', credential_id='test')
     # await private_client.create_market_buy_order('BNB/USDT', 10)
     # pprint(await private_client.fetch_total_balance())
@@ -14,4 +14,5 @@ async def main():
     # pprint(await private_client.fetch_my_trades('BNB/USDT'))
     # await ef.close_all_exchages()
     # pprint(exchange.get_timeframes())
-    pprint(await public_client.fetch_ticker(symbol='BTC/USDT'))
+    # pprint(await public_client.fetch_ticker(symbol='BTC/USDT'))
+    pass
