97268f0d8b89a5c339d207757323a514d2c95a33
Author Name <mohamad97mj@gmail.com>
2021-06-20 01:44:37 +0430

    trade_env was successfully configured

--- trader/clients/private_client.py
+++ trader/clients/private_client.py
@@ -8,35 +8,35 @@ class PrivateClient:
         self._exchange = ef.create_exchange(exchange_id=exchange_id,
                                             credential_id=credential_id)
 
-    async def fetch_total_balance(self):
-        return await self._exchange.fetch_total_balance()
+    def fetch_total_balance(self):
+        return self._exchange.fetch_total_balance()
 
-    async def fetch_orders(self, symbol='BTC/USDT'):
-        return await self._exchange.fetch_orders()
+    def fetch_orders(self, symbol='BTC/USDT'):
+        return self._exchange.fetch_orders()
 
-    async def fetch_order(self, order_id, symbol='BTC/USDT'):
-        return await self._exchange.fetch_orders()
+    def fetch_order(self, order_id, symbol='BTC/USDT'):
+        return self._exchange.fetch_orders()
 
-    async def fetch_open_orders(self, symbol='BTC/USDT'):
-        return await self._exchange.fetch_open_orders(symbol=symbol)
+    def fetch_open_orders(self, symbol='BTC/USDT'):
+        return self._exchange.fetch_open_orders(symbol=symbol)
 
-    async def fetch_closed_orders(self):
-        return await self._exchange.fetch_closed_orders()
+    def fetch_closed_orders(self):
+        return self._exchange.fetch_closed_orders()
 
-    async def fetch_my_trades(self, symbol='BTC/USDT'):
-        return await self._exchange.fetch_my_trades(symbol=symbol)
+    def fetch_my_trades(self, symbol='BTC/USDT'):
+        return self._exchange.fetch_my_trades(symbol=symbol)
 
-    async def create_market_buy_order(self, symbol, amount):
-        return await self._exchange.create_market_buy_order(symbol=symbol, amount=amount)
+    def create_market_buy_order(self, symbol, amount):
+        return self._exchange.create_market_buy_order(symbol=symbol, amount=amount)
 
-    async def create_market_sell_order(self, symbol, amount):
-        return await self._exchange.create_market_sell_order(symbol=symbol, amount=amount)
+    def create_market_sell_order(self, symbol, amount):
+        return self._exchange.create_market_sell_order(symbol=symbol, amount=amount)
 
-    async def create_limit_buy_order(self, symbol, amount, price):
-        return await self._exchange.create_limit_buy_order(symbol=symbol, amount=amount, price=price)
+    def create_limit_buy_order(self, symbol, amount, price):
+        return self._exchange.create_limit_buy_order(symbol=symbol, amount=amount, price=price)
 
-    async def create_limit_sell_order(self, symbol, amount, price):
-        return await self._exchange.create_limit_sell_order(symbol=symbol, amount=amount, price=price)
+    def create_limit_sell_order(self, symbol, amount, price):
+        return self._exchange.create_limit_sell_order(symbol=symbol, amount=amount, price=price)
 
-        # async def create_market_buy_order_in_quote(self, symbol, amount):
-        #     return await self._exchange.create_market_buy_order()
+        #  def create_market_buy_order_in_quote(self, symbol, amount):
+        #     return  self._exchange.create_market_buy_order()
