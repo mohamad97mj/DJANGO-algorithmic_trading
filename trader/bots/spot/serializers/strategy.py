effa2c432f39a04cbb4bbd999ab54ae98dfee7a5
mohamad <mohamad97mj@gmail.com>
2021-06-19 20:14:29 +0430

    ids from serializer was removed and create methods was completed

--- trader/bots/spot/serializers/strategy.py
+++ trader/bots/spot/serializers/strategy.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from trader.models import SpotStrategy
 
 
 class SpotStrategySerializer(serializers.Serializer):
     name = serializers.CharField(max_length=200)
     status = serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        now = datetime.now()
+        strategy_id = '{}|{}'.format(self.symbol, now)
+        return SpotStrategy(strategy_id=strategy_id, **validated_data)
