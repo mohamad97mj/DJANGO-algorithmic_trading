ffb38815fc8a774be684c693bd2ce98637b7bd9c
Author Name <mohamad97mj@gmail.com>
2021-06-24 03:10:44 +0430

    bugs in position view was fixed

--- trader/main/spot/models/utils/strategy_center.py
+++ trader/main/spot/models/utils/strategy_center.py
@@ -57,8 +57,8 @@ class TrailingStoplossStrategyDeveolper:
             # 0.001,
             # 0.005,
             0.01,
-            0.02,
-            0.05,
+            # 0.02,
+            # 0.05,
             # 0.1,
             # 0.2
         ]
@@ -69,15 +69,15 @@ class TrailingStoplossStrategyDeveolper:
             # 0.01,
             # 0.05,
             0.1,
-            0.2,
-            0.25,
+            # 0.2,
+            # 0.25,
         ]
 
         stoploss_safety_ratios = [
-            0,
-            0.2,
+            # 0,
+            # 0.2,
             0.25,
-            0.3
+            # 0.3
         ]
 
         fee = 0.001
