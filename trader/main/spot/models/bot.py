1107e7f2d1331688bbb02a530db71eed3128d517
mohamad <mohamad97mj@gmail.com>
2021-06-24 02:10:42 +0430

    strategy was moved to bot model

--- trader/main/spot/models/bot.py
+++ trader/main/spot/models/bot.py
@@ -20,6 +20,7 @@ class SpotBot(models.Model):
     bot_id = models.CharField(max_length=100, unique=True)
     exchange_id = models.CharField(max_length=100)
     credential_id = models.CharField(max_length=100)
+    strategy = models.CharField(max_length=100)
     position = models.OneToOneField('SpotPosition', related_name='bot', on_delete=models.RESTRICT)
 
     def __init__(self,
