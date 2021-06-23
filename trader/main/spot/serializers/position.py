36b149a882f0f9ab6ef3e8b06d82afee45a5f95e
Author Name <mohamad97mj@gmail.com>
2021-06-24 01:26:42 +0430

    allow_null, required was set for signal in SpotPositionSerializer

--- trader/main/spot/serializers/position.py
+++ trader/main/spot/serializers/position.py
@@ -5,14 +5,16 @@ from ..models import SpotPosition
 
 
 class SpotPositionSerializer(serializers.Serializer):
-    signal = SpotSignalSerializer()
+    signal = SpotSignalSerializer(required=False, allow_null=True)
     volume = serializers.FloatField()
     strategy = serializers.CharField(max_length=100)
 
     def create(self, validated_data):
-        signal_serializer = SpotSignalSerializer(data=validated_data['signal'])
-        if signal_serializer.is_valid():
-            signal = signal_serializer.save()
+        signal = None
+        if validated_data['signal']:
+            signal_serializer = SpotSignalSerializer(data=validated_data['signal'])
+            if signal_serializer.is_valid():
+                signal = signal_serializer.save()
 
         return SpotPosition(signal=signal,
                             **{k: validated_data[k] for k in ['volume', 'strategy']}
