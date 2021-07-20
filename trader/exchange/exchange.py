6d2a64a7a164f575ccb3bcf9e4315550f2ebead9
mohammad <mohamad97mj>
2021-07-21 00:17:07 +0430

    fetch_time was implemented

--- trader/exchange/exchange.py
+++ trader/exchange/exchange.py
@@ -3,9 +3,16 @@ from ccxt.base.errors import ExchangeError
 
 
 class Exchange:
+    default_params = {
+        'recvWindow': 10000
+    }
+
     def __init__(self, ccxt_exchange: ccxt.Exchange, pb_exchange):  # pb stands for python binance
         self._ccxt_exchange = ccxt_exchange
         self._pb_exchange = pb_exchange
+
+    def fetch_time(self):
+        return self._ccxt_exchange.fetch_time()
 
     def load_markets(self, reload):
         return self._ccxt_exchange.load_markets(reload=reload)
@@ -33,7 +40,7 @@ class Exchange:
         if self._ccxt_exchange.has['fetchTickers']:
             return self._ccxt_exchange.fetch_tickers(symbols=symbols)
 
-    def fetck_ticker(self, symbol):
+    def fetch_ticker(self, symbol):
         if self._ccxt_exchange.has['fetchTickers']:
             return self._ccxt_exchange.fetch_ticker(symbol=symbol)
 
@@ -68,8 +75,9 @@ class Exchange:
     def create_market_buy_order(self, symbol, amount):
         return self._ccxt_exchange.create_market_buy_order(symbol=symbol, amount=amount)
 
-    def create_marke_buy_order_in_quote(self, symbol, amount, price):
-        return self._ccxt_exchange.create_order(symbol=symbol, type='market', side='buy', amount=amount, price=price)
+    def create_market_buy_order_in_quote(self, symbol, amount, price):
+        return self._ccxt_exchange.create_order(symbol=symbol, type='market', side='buy', amount=amount, price=price,
+                                                params=self.default_params)
 
     def create_market_sell_order(self, symbol, amount):
         return self._ccxt_exchange.create_market_sell_order(symbol=symbol, amount=amount)
