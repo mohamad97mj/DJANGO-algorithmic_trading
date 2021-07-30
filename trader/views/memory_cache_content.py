4da07cd18da212faab4e1540c5bb4b554583d336
Author Name <mohamad97mj@gmail.com>
2021-07-30 18:04:32 +0430

    memory_cache_content_view was implemented

--- trader/views/memory_cache_content.py
+++ trader/views/memory_cache_content.py
@@ -1,30 +1,20 @@
-import os
 import multiprocessing
-from rest_framework import renderers
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.conf import settings
-from djangorestframework_camel_case import render
-from video_api.global_utils import catch_all_exceptions
+from global_utils import catch_all_exceptions
+from django.core.cache import caches
 
 
 class MemoryCacheContentView(APIView):
-    renderer_classes = (
-        render.CamelCaseJSONRenderer,
-        renderers.BrowsableAPIRenderer,
-    ) if settings.DEBUG else (
-        render.CamelCaseJSONRenderer,
-        renderers.JSONRenderer,
-    )
 
     @catch_all_exceptions(log_level='error', reraise=True)
-    def get(self, request, cache_name, format=None):
+    def get(self, request, cache_name, location, format=None):
         from django.core.cache.backends import locmem
         content = []
-        cache = locmem._caches.get(cache_name, None)
+        keys = locmem._caches[location].copy().keys()
+        cache = caches[cache_name]
         if cache:
-            cache = cache.copy()
-            content = cache.keys()
+            content = {key: cache.get(key) for key in keys}
 
         return Response({
             'count': len(content),
