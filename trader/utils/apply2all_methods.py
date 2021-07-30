adea1da311b290da4e5801c529d3cf04aebe32cf
mohammad <mohamad97mj>
2021-07-30 18:06:17 +0430

    decorators was moved out from decorator dir

--- trader/utils/apply2all_methods.py
+++ trader/utils/apply2all_methods.py
@@ -1,11 +1,9 @@
-import functools
-
-
-def apply2all_methods(decorator):
+def apply2all_methods(*decorators):
     def decorate(cls):
-        for attr in cls.__dict__:
-            if callable(getattr(cls, attr)):
-                setattr(cls, attr, decorator(getattr(cls, attr)))
-        return cls
+        for decorator in decorators:
+            for attr in cls.__dict__:
+                if callable(getattr(cls, attr)):
+                    setattr(cls, attr, decorator(getattr(cls, attr)))
+            return cls
 
     return decorate
