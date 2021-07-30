fd332b49b16eceff148a8fb31e0100d3d4e13571
Author Name <mohamad97mj@gmail.com>
2021-07-30 18:19:35 +0430

    settings for localmemory cache was implemented

--- algorithmic_trading/settings/__init__.py
+++ algorithmic_trading/settings/__init__.py
@@ -1,4 +1,3 @@
 from .main import *
 from .logging import *
-
-
+from .cache import *
--- global_utils/__init__.py
+++ global_utils/__init__.py
@@ -1,2 +1,3 @@
 from .catch_all_exceptions import catch_all_exceptions
 from .my_logging import my_get_logger
+from .retry_on_timeout import retry_on_timeout, async_retry_on_timeout
