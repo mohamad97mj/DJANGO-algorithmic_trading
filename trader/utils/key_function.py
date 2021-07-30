0b6fe6a7b109cf0974d3dd5c5abfbf49079744eb
Author Name <mohamad97mj@gmail.com>
2021-07-30 18:06:17 +0430

    decorators was moved out from decorator dir

--- trader/utils/key_function.py
+++ trader/utils/key_function.py
@@ -1,6 +1,6 @@
 from django.utils.encoding import smart_str
 
 
-def my_key_maker(key, key_prefix, version):
+def my_key_function(key, key_prefix, version):
     new_key = smart_str(''.join([c for c in key if ord(c) > 32 and ord(c) != 127]))
     return new_key
