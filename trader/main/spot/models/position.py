ae823e9a40dacc5ff9df1e9857b1d01e08a39f64
mohammad <mohamad97mj>
2021-08-11 15:49:30 +0430

    position model was refactored

--- trader/main/spot/models/position.py
+++ trader/main/spot/models/position.py
@@ -10,7 +10,7 @@ class SpotPosition(models.Model):
     objects = SpotPositionManager()
 
     # position_id = models.CharField(max_length=100, unique=True)
-    signal = models.ForeignKey('SpotSignal', related_name='positions', on_delete=models.RESTRICT, null=True)
+    signal = models.ForeignKey('SpotSignal', related_name='positions', on_delete=models.CASCADE, null=True)
     volume = models.FloatField()  # initial volume
     # current volume = ?
     created_at = models.CharField(max_length=100, default=timezone.now, blank=True)
