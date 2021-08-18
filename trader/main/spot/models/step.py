8c2803b40936bb91f73844130a18f67d6717a547
Author Name <mohamad97mj@gmail.com>
2021-08-18 07:00:48 +0430

    SpotStep model was implemented

--- trader/main/spot/models/step.py
+++ trader/main/spot/models/step.py
@@ -3,8 +3,9 @@ from django.utils import timezone
 
 
 class SpotStep(models.Model):
-    signal = models.ForeignKey('SpotSignal', related_name='steps', on_delete=models.CASCADE, null=True)
+    signal = models.ForeignKey('SpotSignal', related_name='steps', on_delete=models.CASCADE)
     buy_price = models.FloatField()
-    share = models.FloatField()
+    share = models.FloatField(null=True, blank=True)
     is_triggered = models.BooleanField(default=False)
+    amount_in_quote = models.FloatField(null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now, blank=True)
