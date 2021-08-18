892756320f926ba840c3981ad488edba00190687
mohammad <mohamad97mj>
2021-08-18 12:04:28 +0430

    new routings was added

--- trader/urls.py
+++ trader/urls.py
@@ -5,8 +5,25 @@ from . import views
 app_name = 'trader'
 
 urlpatterns = [
-    path('spot-positions/', views.PositionView.as_view(), name='spot_position'),
-    path('memory-cache-content/<slug:cache_name>/<slug:location>/', views.MemoryCacheContentView.as_view(), name='memory_cache_content')
+    path('spot-bots/',
+         views.SpotBotsView.as_view(),
+         name='spot_bots'),
+
+    path('spot-bots/<slug:bot_id>/',
+         views.SpotBotDetailView.as_view(),
+         name='spot_bot_detail'),
+
+    path('spot-bots/<slug:bot_id>/position/steps/',
+         views.PositionStepView.as_view(),
+         name='spot_bots_position_step'),
+
+    path('spot-bots/<slug:bot_id>/position/targets/',
+         views.PositionStepView.as_view(),
+         name='spot_bots_position_target'),
+
+    path('memory-cache-content/<slug:cache_name>/<slug:location>/',
+         views.MemoryCacheContentView.as_view(),
+         name='memory_cache_content')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
