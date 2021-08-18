91fdcfd5d38475f7e96104a19ac2cbf7a7e9a36a
mohammad <mohamad97mj>
2021-08-18 11:59:44 +0430

    SpotTragetSerializer was implemented

--- trader/main/spot/serializers/target.py
+++ trader/main/spot/serializers/target.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from .signal import SpotSignalSerializer
 
 
-class SpotStepSerializer(serializers.Serializer):
-    signal = SpotSignalSerializer()
-    share = serializers.FloatField()
+class SpotTargetSerializer(serializers.Serializer):
+    tp_price = serializers.FloatField()
+    share = serializers.FloatField(required=False, allow_null=True)
+    amount = serializers.FloatField(required=False)
+    is_triggered = serializers.BooleanField(default=False)
