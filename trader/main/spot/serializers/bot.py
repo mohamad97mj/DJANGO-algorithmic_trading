192ca2ade3be7cf02bdacb438e1d96bdba43b3ba
mohammad <mohamad97mj>
2021-08-18 11:57:53 +0430

    SpotBotSerializer was refactored

--- trader/main/spot/serializers/bot.py
+++ trader/main/spot/serializers/bot.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 from rest_framework import serializers
 from .position import SpotPositionSerializer
-from ..models import SpotPosition
 
 
 class SpotBotSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
     exchange_id = serializers.CharField(max_length=100)
     credential_id = serializers.CharField(max_length=100)
     strategy = serializers.CharField(max_length=100)
     position = SpotPositionSerializer()
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['strategy_state_data'] = instance.strategy_state_data.to_json()
+        return ret
