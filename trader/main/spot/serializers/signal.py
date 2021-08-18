042f215e60e126b4803f17a1dfbe98648766ac9a
mohammad <mohamad97mj>
2021-08-18 21:40:01 +0430

    stpes and targes are sorted in SpotSignalSerializer now

--- trader/main/spot/serializers/signal.py
+++ trader/main/spot/serializers/signal.py
@@ -5,8 +5,14 @@ from .target import SpotTargetSerializer
 
 class SpotSignalSerializer(serializers.Serializer):
     symbol = serializers.CharField(max_length=50)
-    step_share_set_mode = serializers.CharField(max_length=50)
+    step_share_set_mode = serializers.CharField(max_length=50, required=False, allow_null=True)
     steps = SpotStepSerializer(many=True)
-    target_share_set_mode = serializers.CharField(max_length=50)
-    targets = SpotTargetSerializer(many=True)
+    target_share_set_mode = serializers.CharField(max_length=50, required=False, allow_null=True)
+    targets = SpotTargetSerializer(many=True, required=False, allow_null=True)
     stoploss = serializers.FloatField(required=False, allow_null=True)
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['steps'] = sorted(ret['steps'], key=lambda s: s['buy_price'])
+        ret['targets'] = sorted(ret['targets'], key=lambda s: s['tp_price'])
+        return ret
