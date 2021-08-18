f01def94fdf1551a47e58a84ca7cb894a4730168
Author Name <mohamad97mj@gmail.com>
2021-08-18 07:01:23 +0430

    SpotTarget model was implemented

--- trader/main/spot/models/target.py
+++ trader/main/spot/models/target.py
@@ -3,8 +3,9 @@ from django.utils import timezone
 
 
 class SpotTarget(models.Model):
-    signal = models.ForeignKey('SpotSignal', related_name='targets', on_delete=models.CASCADE, null=True)
+    signal = models.ForeignKey('SpotSignal', related_name='targets', on_delete=models.CASCADE)
     tp_price = models.FloatField()
-    share = models.FloatField()
+    share = models.FloatField(null=True, blank=True)
     is_triggered = models.BooleanField(default=False)
+    amount = models.FloatField(default=0)
     created_at = models.DateTimeField(default=timezone.now, blank=True)
