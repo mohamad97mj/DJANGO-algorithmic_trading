8414426addc0c738bcc29d657959d10ba050ce33
mohammad <mohamad97mj>
2021-08-18 06:48:02 +0430

    edit_none_triggered_steps was implemented in spot trader

--- trader/main/spot/trader.py
+++ trader/main/spot/trader.py
@@ -7,11 +7,19 @@ class SpotTrader:
     def __init__(self):
         self._bot_handler = SpotBotHandler()
 
-    def open_position(self, exchange_id: str, credential_id: str, strategy: str, position: SpotPosition):
+    def create_bot(self, exchange_id: str, credential_id: str, strategy: str, position_data: dict):
         return self._bot_handler.create_bot(exchange_id=exchange_id,
                                             credential_id=credential_id,
                                             strategy=strategy,
-                                            position=position)
+                                            position_data=position_data)
+
+    def edit_none_triggered_steps(self, bot_id, *args, **kwargs):
+        return self._bot_handler.run_strategy_developer_command(
+            bot_id,
+            'edit_none_triggered_steps',
+            *args,
+            **kwargs)
+
 
     def start(self):
         self._bot_handler.reload_bots()
