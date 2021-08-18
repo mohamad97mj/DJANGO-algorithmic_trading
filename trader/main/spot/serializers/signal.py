3fbdbbd5ea256640a19bbc76ae8a1bba347786fb
Author Name <mohamad97mj@gmail.com>
2021-08-18 11:58:58 +0430

    SpotSignalSerializer was refactored

--- trader/main/spot/serializers/signal.py
+++ trader/main/spot/serializers/signal.py
@@ -1,15 +1,12 @@
-from datetime import datetime
 from rest_framework import serializers
-from ..models import SpotSignal
+from .step import SpotStepSerializer
+from .target import SpotTargetSerializer
 
 
 class SpotSignalSerializer(serializers.Serializer):
     symbol = serializers.CharField(max_length=50)
-    steps = serializers.ListField(child=serializers.FloatField())
-    targets = serializers.ListField(child=serializers.FloatField())
-    stoploss = serializers.FloatField()
-
-    def create(self, validated_data):
-        spot_signal = SpotSignal(**validated_data)
-        spot_signal.save()
-        return spot_signal
+    step_share_set_mode = serializers.CharField(max_length=50)
+    steps = SpotStepSerializer(many=True)
+    target_share_set_mode = serializers.CharField(max_length=50)
+    targets = SpotTargetSerializer(many=True)
+    stoploss = serializers.FloatField(required=False, allow_null=True)
