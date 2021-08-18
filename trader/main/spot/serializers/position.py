cb9ada12b4eb622a7621a228114799b08621b66b
Author Name <mohamad97mj@gmail.com>
2021-08-18 11:58:37 +0430

    SpotPositionSerializer was refactored

--- trader/main/spot/serializers/position.py
+++ trader/main/spot/serializers/position.py
@@ -1,22 +1,7 @@
-from datetime import datetime
 from rest_framework import serializers
 from .signal import SpotSignalSerializer
-from ..models import SpotPosition
 
 
 class SpotPositionSerializer(serializers.Serializer):
-    signal = SpotSignalSerializer(allow_null=True)
-    volume = serializers.FloatField()
-
-    def create(self, validated_data):
-        signal = None
-        if validated_data['signal']:
-            signal_serializer = SpotSignalSerializer(data=validated_data['signal'])
-            if signal_serializer.is_valid():
-                signal = signal_serializer.save()
-
-        spot_position = SpotPosition(signal=signal,
-                                     **{k: validated_data[k] for k in ['volume']})
-
-        spot_position.save()
-        return spot_position
+    signal = SpotSignalSerializer(required=False, allow_null=True)
+    size = serializers.FloatField()
