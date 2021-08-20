a3bbfd7e40d0e3f48bb84862f745a66d4fc497eb
mohammad <mohamad97mj>
2021-08-20 17:45:21 +0430

    trader was merged with SpotBotService

--- trader/main/spot/views/bot.py
+++ trader/main/spot/views/bot.py
@@ -1,7 +1,7 @@
 from rest_framework import renderers
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..services import SpotBotService
+from ..services import SpotBotTrader
 from ..serializers import SpotBotSerializer
 from global_utils import catch_all_exceptions
 
@@ -15,7 +15,7 @@ class SpotBotsView(APIView):
     @catch_all_exceptions(reraise=True)
     def get(self, request, format=None):
         credential_id = request.query_params.get('credential_id', 'test')
-        bot_instances = SpotBotService.get_active_bots(credential_id=credential_id)
+        bot_instances = SpotBotTrader.get_active_bots(credential_id=credential_id)
         return Response(data=SpotBotSerializer(bot_instances, many=True).data)
 
     @catch_all_exceptions(reraise=True)
@@ -29,7 +29,7 @@ class SpotBotsView(APIView):
 
         test_data = None
         if bot_serializer.is_valid():
-            bot_instance = SpotBotService.create_bot(bot_serializer.validated_data)
+            bot_instance = SpotBotTrader.create_bot(bot_serializer.validated_data)
             test_data = SpotBotSerializer(instance=bot_instance).data
 
         return Response(test_data)
@@ -38,10 +38,10 @@ class SpotBotsView(APIView):
 class SpotBotDetailView(APIView):
     @catch_all_exceptions(reraise=True)
     def get(self, request, bot_id, format=None):
-        bot_instance = SpotBotService.get_bot(bot_id=bot_id)
+        bot_instance = SpotBotTrader.get_bot(bot_id=bot_id)
         return Response(data=SpotBotSerializer(instance=bot_instance).data)
 
     def delete(self, request, bot_id, format=None):
-        bot_instance = SpotBotService.stop_bot(bot_id)
+        bot_instance = SpotBotTrader.stop_bot(bot_id)
         return Response(data=SpotBotSerializer(instance=bot_instance).data)
 
