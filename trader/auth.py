271418986f9ff293b28cf74a3e76d40a346d4eec
Author Name <mohamad97mj@gmail.com>
2021-08-26 11:35:26 +0430

    test

--- trader/auth.py
+++ trader/auth.py
@@ -1,12 +1,20 @@
 import os
 
 credentials = {
-    'main': {
-        'api_key': os.environ.get('MAIN_API_KEY'),
-        'secret_key': os.environ.get('MAIN_SECRET_KEY')
+    'binance_main': {
+        'api_key': os.environ.get('BINANCE_MAIN_API_KEY'),
+        'secret': os.environ.get('BINANCE_MAIN_SECRET')
     },
-    'test': {
-        'api_key': os.environ.get('TEST_API_KEY'),
-        'secret_key': os.environ.get('TEST_SECRET_KEY')
+    'binance_test': {
+        'api_key': os.environ.get('BINANCE_TEST_API_KEY'),
+        'secret': os.environ.get('BINANCE_TEST_SECRET')
+    },
+    'kucoin_main': {
+        'api_key': os.environ.get('KUCOIN_MAIN_API_KEY'),
+        'secret': os.environ.get('KUCOIN_MAIN_SECRET')
+    },
+    'kucoin_test': {
+        'api_key': os.environ.get('KUCOIN_TEST_API_KEY'),
+        'secret': os.environ.get('KUCOIN_TEST_SECRET')
     }
 }
