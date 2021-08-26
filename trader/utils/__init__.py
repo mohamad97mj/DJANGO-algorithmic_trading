3bd39e75012f6e8b58fd20541d9a15a0086d8d34
mohammad <mohamad97mj@gmail.com>
2021-08-26 21:07:13 +0430

    slash2dash was implemented

--- trader/utils/__init__.py
+++ trader/utils/__init__.py
@@ -2,7 +2,7 @@ from .custom_exception import CustomException
 from .truncate import truncate
 from .copy import my_copy
 from .list import union, intersection
-from .symbol_converter import with2without_slash, without2with_slash
+from .symbol_converter import with2without_slash, without2with_slash, slash2dash
 from .log import log
 from .apply2all_methods import apply2all_methods
 from .cache import CacheUtils
--- trader/utils/symbol_converter.py
+++ trader/utils/symbol_converter.py
@@ -9,3 +9,7 @@ def without2with_slash(symbol, market):
     with_slash_symbols = list(market.keys)
     without2with_slash_mapper = {s.replace('/', ''): s for s in with_slash_symbols}
     return without2with_slash_mapper[symbol]
+
+
+def slash2dash(symbol: str):
+    return symbol.replace('/', '-')
