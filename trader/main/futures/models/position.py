85f1112089ba32967d946b80eefa618a68899d30
mohammad <mohamad97mj@gmail.com>
2021-09-01 20:42:41 +0430

    position model was implemented

---
+++ trader/main/futures/models/position.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+
+class FuturesPositionManager(models.Manager):
+    pass
+
+
+class FuturesPosition(models.Model):
+    objects = FuturesPositionManager()
+
+    # position_id = models.CharField(max_length=100, unique=True)
+    signal = models.ForeignKey('FuturesSignal', related_name='positions', on_delete=models.CASCADE, null=True, blank=True)
+    size = models.FloatField()  # initial volume
+    # current volume = ?
+    keep_open = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now, blank=True)
