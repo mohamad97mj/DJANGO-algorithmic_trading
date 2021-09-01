cc35e57a65ebf0c5dc41b7ebf6d7b7155be3ba2c
mohammad <mohamad97mj@gmail.com>
2021-09-01 21:06:23 +0430

    sdk_exchange futrues was implemented

---
+++ trader/main/futures/exchange/sdk_exchange.py
@@ -0,0 +1,12 @@
+from kucoin_futures import client
+
+
+class SdkExchange:
+    def __init__(self, api_key, secret, password, is_sandbox):
+        self.market_client = client.Market(is_sandbox=is_sandbox)
+        self.trade_client = client.Trade(key=api_key,
+                                         secret=secret,
+                                         passphrase=password,
+                                         is_sandbox=is_sandbox,)
+        self.user_client = client.User(key=api_key, secret=secret, passphrase=password, is_sandbox=is_sandbox)
+
