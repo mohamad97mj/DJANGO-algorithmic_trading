fa8fccb1bb4bef90fcd0b2d75763f5d1916fbda5
mohammad <mohamad97mj@gmail.com>
2021-09-02 20:47:07 +0430

    position serializer was refactored for futures

---
+++ trader/main/futures/serializers/position.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .signal import FuturesSignalSerializer
+
+
+class FuturesPositionSerializer(serializers.Serializer):
+    signal = FuturesSignalSerializer(required=False, allow_null=True)
+    size = serializers.FloatField(required=False, allow_null=True)
+    leverage = serializers.FloatField()
+    cost = serializers.FloatField(required=False, allow_null=True)
+    keep_open = serializers.BooleanField(required=False, allow_null=True)
