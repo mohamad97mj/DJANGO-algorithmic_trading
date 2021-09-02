47df965abb2148158513a803e8ba06e3e33397ce
mohammad <mohamad97mj@gmail.com>
2021-09-02 10:50:49 +0430

    get_order was implemented in Futures exchange

--- trader/main/futures/exchange/exchange.py
+++ trader/main/futures/exchange/exchange.py
@@ -16,15 +16,21 @@ class Exchange:
         self._exchange_id = exchange_id
         self._sdk_exchange = sdk_exchange
 
+    def get_order(self, order_id):
+        return self._sdk_exchange.trade_client.get_order_details(order_id=order_id)
+
     def create_market_buy_order(self, symbol, leverage, size):
-        self._sdk_exchange.trade_client.create_market_order(symbol=symbol,
-                                                            side='buy',
-                                                            lever=leverage,
-                                                            size=size)
+        exchange_order = self._sdk_exchange.trade_client.create_market_order(symbol=symbol,
+                                                                             side='buy',
+                                                                             lever=leverage,
+                                                                             size=size)
+        return self.get_order(exchange_order['orderId'])
 
     def create_market_sell_order(self, symbol, leverage, size):
-        self._sdk_exchange.trade_client.create_market_order(symbol=symbol,
-                                                            side='sell',
-                                                            lever=leverage,
-                                                            size=size)
+        exchange_order = self._sdk_exchange.trade_client.create_market_order(symbol=symbol,
+                                                                             side='sell',
+                                                                             lever=leverage,
+                                                                             size=size)
+        return self.get_order(exchange_order['orderId'])
+
 
