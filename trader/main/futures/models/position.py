a9967385b5465ddfa43cacd6d38d5a5bc6d831bd
mohammad <mohamad97mj@gmail.com>
2021-09-02 20:45:19 +0430

    position model was refactored for futures

--- trader/main/futures/models/position.py
+++ trader/main/futures/models/position.py
@@ -12,6 +12,6 @@ class FuturesPosition(models.Model):
     # position_id = models.CharField(max_length=100, unique=True)
     signal = models.ForeignKey('FuturesSignal', related_name='positions', on_delete=models.CASCADE, null=True, blank=True)
     size = models.FloatField()  # initial volume
-    # current volume = ?
+    leverage = models.IntegerField()
     keep_open = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now, blank=True)
