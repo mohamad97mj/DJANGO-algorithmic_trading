429e7233ad0ac0af43300054f34c1a16ac92f4f4
mohammad <mohamad97mj@gmail.com>
2021-09-02 16:39:51 +0430

    public client was implemented for futures

--- trader/main/futures/clients/public_client.py
+++ trader/main/futures/clients/public_client.py
@@ -1,62 +1,15 @@
-from trader.main.spot.exchange import ef
+from trader.main.futures.exchange import ef
 
 
 class PublicClient:
     def __init__(self, exchange_id):
         self._exchange = ef.create_exchange(exchange_id=exchange_id)
 
-        # def get_exchanges(self):
-        #     return ccxt.exchanges
-
-    def fetch_status(self):
-        return self._exchange.fetch_status()
-
-    def fetch_time(self):
-        return self._exchange.fetch_time()
-
-    def load_markets(self, reload=False):
-        return self._exchange.load_markets(reload=reload)
-
-    def get_markets(self):
-        return self._exchange.get_markets()
-
-    def get_market(self, symbol='BTC/USDT'):
-        return self._exchange.get_market(symbol=symbol)
-
-    def get_symbols(self):
-        return self._exchange.get_symbols()
-
-    def fetch_order_book(self, symbol='BTC/USDT', limit=None):
-        return self._exchange.fetch_order_book(symbol=symbol, limit=limit)
-
-    def fetch_tickers(self, symbols=None):
-        if symbols is None:
-            symbols = ['BTC/USDT', 'ETH/USDT']
-
-        return self._exchange.fetch_tickers(symbols=symbols)
-
     def fetch_ticker(self, symbol='BTC/USDT'):
         return self._exchange.fetch_ticker(symbol=symbol)
 
-    def fetch_ohlcv(self, symbol='BTC/USDT', timeframe='1m', since=None, limit=None):
-        if not limit:
-            limit = get_appropriate_limit(timeframe)
-        return self._exchange.fetch_ohlcv(symbol=symbol, timeframe=timeframe, since=since, limit=limit)
+    def get_contracts(self):
+        return self._exchange.get_contracts()
 
-    def fetch_status(self):
-        return self._exchange.fetch_status()
-
-
-def get_appropriate_limit(timeframe):
-    return int({
-                   '1m': 24 * 60,
-                   '3m': (24 * 60) / 3,
-                   '5m': (24 * 60) / 5,
-                   '15m': (24 * 60) / 15,
-                   '30m': (24 * 60) / 30,
-                   '1h': 24,
-                   '2h': 12,
-                   '4h': 6,
-                   '6h': 4,
-                   '8h': 3,
-               }[timeframe])
+    def get_contract(self, symbol):
+        return self._exchange.get_contract(symbol=symbol)
