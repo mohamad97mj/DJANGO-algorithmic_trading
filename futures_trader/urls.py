bf093786d19951a8070d989c2e39d0a2b72440d5
mohammad <mohamad97mj@gmail.com>
2021-09-03 19:08:41 +0430

    futures urls was added

--- futures_trader/urls.py
+++ futures_trader/urls.py
@@ -2,24 +2,24 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-app_name = 'spot_trader'
+app_name = 'futures_trader'
 
 urlpatterns = [
     path('bots/',
-         views.SpotBotsView.as_view(),
-         name='spot_bots'),
+         views.FuturesBotsView.as_view(),
+         name='futures_bots'),
 
     path('bots/<slug:bot_id>/',
-         views.SpotBotDetailView.as_view(),
-         name='spot_bot_detail'),
+         views.FuturesBotDetailView.as_view(),
+         name='futures_bot_detail'),
 
     path('bots/<slug:bot_id>/position/',
-         views.SpotPositionView.as_view(),
-         name='spot_bots_position'),
+         views.FuturesPositionView.as_view(),
+         name='futures_bots_position'),
 
     path('price-monitor/<slug:cache_name>/<slug:location>/',
-         views.PriceMonitorView.as_view(),
-         name='price_monitor')
+         views.FuturesPriceMonitorView.as_view(),
+         name='futures_price_monitor')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
