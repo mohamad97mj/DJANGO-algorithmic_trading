7fcb47b282ca2147a8e5bc952921aa0869598ee8
mohammad <mohamad97mj@gmail.com>
2021-09-07 16:06:38 +0430

    serializers was refactored

--- futures_trader/serializers/step.py
+++ futures_trader/serializers/step.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 class FuturesStepSerializer(serializers.Serializer):
     buy_price = serializers.FloatField()
     share = serializers.FloatField(required=False, allow_null=True)
+    margin = serializers.FloatField(required=False, allow_null=True)
     size = serializers.FloatField(required=False, allow_null=True)
     cost = serializers.FloatField(required=False, allow_null=True)
     is_triggered = serializers.BooleanField(default=False)
