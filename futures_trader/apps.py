0bd203e4344c592d0edfde1ada3e1d0daed29362
mohammad <mohamad97mj@gmail.com>
2021-09-07 16:08:12 +0430

    apps was implemented

--- futures_trader/apps.py
+++ futures_trader/apps.py
@@ -1,6 +1,15 @@
+import sys
 from django.apps import AppConfig
 
 
 class FuturesTraderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'futures_trader'
+
+    def ready(self):
+        if 'runserver' in sys.argv:
+            from global_utils import my_get_logger
+            logger = my_get_logger()
+            logger.info("futures_trader app started!")
+            from futures_trader.trade import trade
+            trade()
