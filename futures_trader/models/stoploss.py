8c70e256adc7944bb3b7c8b0f36c1c3934a5d293
mohammad <mohamad97mj@gmail.com>
2021-09-08 13:43:15 +0430

    junks was removed from stoploss model

--- futures_trader/models/stoploss.py
+++ futures_trader/models/stoploss.py
@@ -6,7 +6,5 @@ class FuturesStoploss(models.Model):
     trigger_price = models.FloatField()
     is_triggered = models.BooleanField(default=False)
     is_trailed = models.BooleanField(default=False)
-    size = models.FloatField(default=0)
-    released_margin = models.FloatField(default=0)
     operation = models.OneToOneField('FuturesOperation', related_name='stoploss', on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(default=timezone.now, blank=True)
