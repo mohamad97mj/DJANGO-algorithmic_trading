0669397665b3f3b124a3349a5ae3bb0bf34788f0
mohammad <mohamad97mj@gmail.com>
2021-09-10 21:43:31 +0430

    signal serializer was changed to support short positions

--- futures_trader/serializers/signal.py
+++ futures_trader/serializers/signal.py
@@ -6,6 +6,7 @@ from .stoploss import FuturesStoplossSerializer
 
 class FuturesSignalSerializer(serializers.Serializer):
     symbol = serializers.CharField(max_length=50)
+    side = serializers.CharField(max_length=10)
     step_share_set_mode = serializers.CharField(max_length=50, required=False, allow_null=True)
     steps = FuturesStepSerializer(many=True)
     targets = FuturesTargetSerializer(many=True, required=False, allow_null=True)
@@ -13,9 +14,10 @@ class FuturesSignalSerializer(serializers.Serializer):
 
     def to_representation(self, instance):
         ret = super().to_representation(instance)
-        sorted_steps = sorted(ret['steps'], key=lambda s: s['buy_price'])
-        if sorted_steps[0]['buy_price'] == -1:
+        is_reversed = instance.side == 'sell'
+        sorted_steps = sorted(ret['steps'], reverse=is_reversed, key=lambda s: s['entry_price'])
+        if sorted_steps[0]['entry_price'] == -1:
             sorted_steps.append(sorted_steps.pop(0))
         ret['steps'] = sorted_steps
-        ret['targets'] = sorted(ret['targets'], key=lambda s: s['tp_price'])
+        ret['targets'] = sorted(ret['targets'], reverse=is_reversed, key=lambda s: s['tp_price'])
         return ret
