a0163834f0313c129b0da1f4b9b848bf1246c2a4
mohammad <mohamad97mj@gmail.com>
2021-09-10 23:34:03 +0430

    kyes hard coded

--- futures_trader/auth.py
+++ futures_trader/auth.py
@@ -2,9 +2,9 @@ import os
 
 futures_credentials = {
     'kucoin_main': {
-        'api_key': os.environ.get('KUCOIN_FUTURES_MAIN_API_KEY'),
-        'secret': os.environ.get('KUCOIN_FUTURES_MAIN_SECRET'),
-        'password': os.environ.get('KUCOIN_FUTURES_MAIN_PASSWORD')
+        'api_key': os.environ.get('KUCOIN_FUTURES_MAIN_API_KEY', '612f3c39d0d4e100068f297a'),
+        'secret': os.environ.get('KUCOIN_FUTURES_MAIN_SECRET', '0db7f3fe-2d09-4c27-90b0-661cae4eeb81'),
+        'password': os.environ.get('KUCOIN_FUTURES_MAIN_PASSWORD', 'mJ604998')
     },
     'kucoin_test': {
         'api_key': os.environ.get('KUCOIN_FUTURES_TEST_API_KEY'),
