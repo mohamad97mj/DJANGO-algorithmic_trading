b06eac19dcadfd3b0577db58be4817e30c02e5c2
mohammad <mohamad97mj@gmail.com>
2021-09-10 17:46:38 +0430

    junks was removed

--- futures_trader/views/price_monitor.py
+++ futures_trader/views/price_monitor.py
@@ -1,7 +1,7 @@
 import multiprocessing
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from global_utils import catch_all_exceptions, my_get_logger
+from global_utils import catch_all_exceptions
 from django.core.cache import caches
 
 
@@ -11,8 +11,6 @@ class FuturesPriceMonitorView(APIView):
     def get(self, request, cache_name, location, format=None):
         from django.core.cache.backends import locmem
         content = []
-        logger = my_get_logger()
-        logger.debug(locmem._caches)
         keys = locmem._caches[location].copy().keys()
         cache = caches[cache_name]
         if cache:
