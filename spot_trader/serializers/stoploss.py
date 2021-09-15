7ab2cca3ff6df8ddd3b93dfe591b9af823983f62
mohammad <mohamad97mj@gmail.com>
2021-09-15 13:59:28 +0430

    small changes in stoploss serializre

--- spot_trader/serializers/stoploss.py
+++ spot_trader/serializers/stoploss.py
@@ -3,5 +3,4 @@ from rest_framework import serializers
 
 class SpotStoplossSerializer(serializers.Serializer):
     trigger_price = serializers.FloatField()
-    amount = serializers.FloatField(required=False)
     is_triggered = serializers.BooleanField(default=False)
