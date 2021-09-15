f81427301a35d20967359718c7b9a22b8d8f18b2
mohammad <mohamad97mj@gmail.com>
2021-09-15 13:52:42 +0430

    futures stoploss serializer was refactored

--- futures_trader/serializers/stoploss.py
+++ futures_trader/serializers/stoploss.py
@@ -3,5 +3,4 @@ from rest_framework import serializers
 
 class FuturesStoplossSerializer(serializers.Serializer):
     trigger_price = serializers.FloatField()
-    size = serializers.FloatField(required=False)
     is_triggered = serializers.BooleanField(default=False)
