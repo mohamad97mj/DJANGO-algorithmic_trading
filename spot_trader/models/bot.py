a96ee712d885bd3e3005d3bf8a613fb3311e7178
mohammad <mohamad97mj@gmail.com>
2021-09-15 13:57:20 +0430

    small changes in model

--- spot_trader/models/bot.py
+++ spot_trader/models/bot.py
@@ -1,3 +1,5 @@
+import time
+
 from global_utils import my_get_logger
 from typing import List, Union
 from django.utils import timezone
@@ -5,10 +7,6 @@ from django.db import models
 from spot_trader.clients import PrivateClient, PublicClient
 from global_utils import BotDoesNotExistsException, truncate
 from .operation import SpotOperation
-from dataclasses import dataclass
-from .step import SpotStep
-from .target import SpotTarget
-from .stoploss import SpotStoploss
 
 
 class SpotBotManager(models.Manager):
@@ -28,15 +26,6 @@ class SpotBot(models.Model):
         # STOPPED_BY_TRAILING_STOPLOSS = 'stopped_by_trailing_stoploss'
         STOPPED_AFTER_FULL_TARGET = 'stopped_after_full_target'
         STOPPED_MANUALY = 'stopped_manualy'
-
-    @dataclass
-    class ExchangeOrderData:
-        side: str
-        symbol: str
-        cost: float
-        fee: float
-        amount: float
-        related_setup: Union[SpotStep, SpotTarget, SpotStoploss]  # could be step_id, target_id or stoploss_id
 
     objects = SpotBotManager()
 
@@ -91,90 +80,91 @@ class SpotBot(models.Model):
                                 exchange_order['price'],
                                 balance))
 
-    def execute_operations(self, operations: List[SpotOperation], symbol_prices=None, test=True):
-        exchange_orders_data = []
+    def execute_operations(self,
+                           operations: List[SpotOperation],
+                           test=True):
         for operation in operations:
-            exchange_order_data = None
+            position = operation.position
+            signal = position.signal
+            order = operation.order
+            symbol = order.symbol
+            price = order.price
+            markets = self._public_client.get_markets()
+            amount_precision = markets[symbol]['precision']['amount']
+            fee = markets[symbol]['maker']
             if operation.action == 'create':
-                if operation.order.type == 'market':
+                if order.side == 'buy':
+                    step = operation.step
+                    if test:
+                        exchange_order_id = None
+                        timestamp = time.time()
+                        buy_amount_in_quote = order.amount_in_quote
+                        truncated_buy_amount_before_fee = truncate(
+                            buy_amount_in_quote / price,
+                            amount_precision)
+                        fee_amount = truncated_buy_amount_before_fee * fee
+                        cost = truncated_buy_amount_before_fee * price
+                        amount = truncated_buy_amount_before_fee - fee_amount
+                    else:
+                        exchange_order = self._private_client.create_market_buy_order_in_quote(
+                            symbol=symbol,
+                            amount_in_quote=order.amount_in_quote)
+                        timestamp = exchange_order['createdAt']
+                        cost = exchange_order['cost']
+                        amount = exchange_order['filled']
+                        price = exchange_order['price']
+                        exchange_order_id = exchange_order['id']
 
-                    symbol = operation.order.symbol
-                    markets = self._public_client.get_markets()
-                    amount_precision = markets[symbol]['precision']['amount']
-                    fee = markets[symbol]['maker']
-                    deviation = 0.0001
+                    step.purchased_amount = amount
+                    step.amount_in_quote -= cost
+                    step.save()
 
-                    if operation.order.side == 'buy':
-                        related_setup = operation.step
-                        if test:
-                            buy_amount_in_quote = operation.order.amount_in_quote
-                            buy_price = symbol_prices[symbol]
-                            truncated_buy_amount_before_fee = truncate(
-                                buy_amount_in_quote / (buy_price * (1 + deviation)),
-                                amount_precision)
-                            fee_amount = truncated_buy_amount_before_fee * fee
-                            real_buy_amount_in_quote = truncated_buy_amount_before_fee * buy_price
-                            exchange_order_data = SpotBot.ExchangeOrderData(side='buy',
-                                                                            symbol=symbol,
-                                                                            cost=real_buy_amount_in_quote,
-                                                                            amount=truncated_buy_amount_before_fee,
-                                                                            fee=fee_amount,
-                                                                            related_setup=related_setup)
-                            pure_buy_amount = truncated_buy_amount_before_fee - fee_amount
-                            operation.step.purchased_amount = pure_buy_amount
-                        else:
-                            exchange_order = self._private_client.create_market_buy_order_in_quote(
-                                symbol=symbol,
-                                amount_in_quote=operation.order.amount_in_quote)
-                            exchange_order_data = SpotBot.ExchangeOrderData(side='buy',
-                                                                            symbol=symbol,
-                                                                            cost=exchange_order['cost'],
-                                                                            amount=exchange_order['amount'],
-                                                                            fee=exchange_order['fee']['cost'],
-                                                                            related_setup=related_setup)
+                    position.holding_amount += amount
 
-                            pure_buy_amount = exchange_order['amount'] - exchange_order['fee']['cost']
-                            operation.step.purchased_amount = pure_buy_amount
-                        operation.step.save()
+                    targets = signal.related_targets
+                    for target in targets:
+                        target.holding_amount += amount * target.share
+                        target.save()
 
-                    elif operation.order.side == 'sell':
-                        related_setup = operation.stoploss if operation.type == 'stoploss_triggered' else operation.target
-                        if test:
+                else:
+                    if test:
+                        exchange_order_id = None
+                        sell_amount = order.amount
+                        timestamp = time.time()
+                        amount = truncate(sell_amount, amount_precision)
+                        sell_amount_in_quote = amount * price
+                        fee_amount_in_quote = sell_amount_in_quote * fee
+                        pure_sell_amount_in_quote = sell_amount_in_quote - fee_amount_in_quote
 
-                            sell_amount = operation.order.amount
-                            sell_price = symbol_prices[symbol]
-                            truncated_sell_amount_before_fee = truncate(sell_amount, amount_precision)
-                            sell_amount_in_quote = truncated_sell_amount_before_fee * (sell_price * (1 - deviation))
-                            fee_amount_in_quote = sell_amount_in_quote * fee
-                            exchange_order_data = SpotBot.ExchangeOrderData(side='sell',
-                                                                            symbol=symbol,
-                                                                            cost=sell_amount_in_quote,
-                                                                            fee=fee_amount_in_quote,
-                                                                            amount=truncated_sell_amount_before_fee,
-                                                                            related_setup=related_setup)
+                    else:
+                        exchange_order = self._private_client.create_market_sell_order(
+                            symbol=symbol,
+                            amount=order.amount)
 
-                            pure_sell_amount_in_quote = sell_amount_in_quote - fee_amount_in_quote
-                            related_setup.released_amount_in_quote = pure_sell_amount_in_quote
+                        amount = exchange_order['filled']
+                        timestamp = exchange_order['createdAt']
+                        pure_sell_amount_in_quote = exchange_order['cost'] - exchange_order['fee']['cost']
+                        price = exchange_order['price']
+                        exchange_order_id = exchange_order['id']
+                    position.released_amount_in_quote += pure_sell_amount_in_quote
+                    position.holding_amount -= amount
 
-                        else:
-                            exchange_order = self._private_client.create_market_sell_order(
-                                symbol=symbol,
-                                amount=operation.order.amount)
-                            exchange_order_data = SpotBot.ExchangeOrderData(side='sell',
-                                                                            symbol=symbol,
-                                                                            cost=exchange_order['cost'],
-                                                                            fee=exchange_order['fee']['cost'],
-                                                                            amount=exchange_order['amount'],
-                                                                            related_setup=related_setup)
-                            pure_sell_amount_in_quote = exchange_order['cost'] - exchange_order['fee']['cost']
-                            related_setup.released_amount_in_quote = pure_sell_amount_in_quote
-                            related_setup.save()
+                    if operation.type == 'take_profit':
+                        target = operation.target
+                        target.holding_amount -= amount
+                        target.released_amount_in_quote = pure_sell_amount_in_quote
+                        target.save()
 
-            logger = my_get_logger()
-            logger.info('exchange_order: {}'.format(exchange_order_data))
-            exchange_orders_data.append(exchange_order_data)
+                order.exchange_order_id = exchange_order_id
+                order.timestamp = timestamp
+                order.status = 'done'
+                order.price = price
+                order.save()
 
-        return exchange_orders_data
+            position.save()
+            # order.
 
-    def close_position(self):
-        self.sell_all_assets()
+    def close_position(self, test):
+        if not test:
+            pass
+            # self.sell_all_assets()
