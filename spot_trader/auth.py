61c174e9825116d51ae7ebc5e5179089e84a4d87
mohammad <mohamad97mj@gmail.com>
2021-09-15 14:01:20 +0430

    keys was hardcoded

--- spot_trader/auth.py
+++ spot_trader/auth.py
@@ -10,9 +10,9 @@ spot_credentials = {
         'secret': os.environ.get('BINANCE_TEST_SECRET')
     },
     'kucoin_main': {
-        'api_key': os.environ.get('KUCOIN_MAIN_API_KEY'),
-        'secret': os.environ.get('KUCOIN_MAIN_SECRET'),
-        'password': os.environ.get('KUCOIN_MAIN_PASSWORD')
+        'api_key': os.environ.get('KUCOIN_MAIN_API_KEY', '61272984300d59000665294e'),
+        'secret': os.environ.get('KUCOIN_MAIN_SECRET', '4d48f02d-9e63-4cff-8eb9-e398c72b48a3'),
+        'password': os.environ.get('KUCOIN_MAIN_PASSWORD', 'mJ604998')
     },
     'kucoin_test': {
         'api_key': os.environ.get('KUCOIN_TEST_API_KEY'),
