3d13338b319d3b5299815d2f563fca616c62506f
mohammad <mohamad97mj@gmail.com>
2021-09-15 13:59:38 +0430

    small changes in target serializre

--- spot_trader/serializers/target.py
+++ spot_trader/serializers/target.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class SpotTargetSerializer(serializers.Serializer):
     tp_price = serializers.FloatField()
     share = serializers.FloatField(required=False, allow_null=True)
-    amount = serializers.FloatField(required=False)
+    purchased_amount = serializers.FloatField(required=False)
     is_triggered = serializers.BooleanField(default=False)
