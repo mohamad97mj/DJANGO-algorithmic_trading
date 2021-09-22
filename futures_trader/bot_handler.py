b324ebe5ecd6c5056ee4901a96eac308692a4593
mohammad <mohamad97mj@gmail.com>
2021-09-22 19:32:51 +0330

    sleep for price ticker was increaded

--- futures_trader/bot_handler.py
+++ futures_trader/bot_handler.py
@@ -157,7 +157,7 @@ class FuturesBotHandler:
                                 self._start_muck_symbols_price_ticker(bot.exchange_id, price_required_symbols)
                             else:
                                 self._start_symbols_price_ticker(bot.exchange_id, price_required_symbols)
-                            time.sleep(15)
+                            time.sleep(25)
                             symbol_prices = self._get_prices_if_available(bot.exchange_id, price_required_symbols)
 
                         logger = my_get_logger()
