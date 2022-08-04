68bceef688b5d65b22753302aa26fecdc3da8c8d
Mojahed <mohamad97mj@gmail.com>
2022-08-04 17:19:42 +0430

    24

--- algorithmic_trading/celery.py
+++ algorithmic_trading/celery.py
@@ -20,7 +20,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'run-every-hour': {
         'task': 'futures_trader.tasks.technical_auto_trade',
-        'schedule': crontab(minute=26),
+        'schedule': crontab(minute=24),
         'options': {
             'expires': 30.0,
         },
