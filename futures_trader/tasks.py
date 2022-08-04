ac567b9277e01107cff18649d2041b301b88e21b
Mojahed <mohamad97mj@gmail.com>
2022-08-04 17:23:42 +0430

    24 and catch all exception

--- futures_trader/tasks.py
+++ futures_trader/tasks.py
@@ -6,6 +6,7 @@ from futures_trader.services.external_signal_trader.auto_trader import create_cl
 from spot_trader.clients import PublicClient
 from futures_trader.services.trader import FuturesBotTrader
 from global_utils.my_logging import my_get_logger
+from global_utils.catch_all_exceptions import catch_all_exceptions
 from time import sleep
 
 myclient = create_client()
@@ -25,78 +26,82 @@ def technical_auto_trade():
     appropriate_longs = []
     appropriate_shorts = []
     for symbol in symbols:
-        print(symbol)
-        logger = my_get_logger()
-        cci, prev_cci = TechnicalAnalyser.get_cci(symbol=symbol,
-                                                  timeframe='1h',
-                                                  n=20)
-        macd, prev_macd = TechnicalAnalyser.get_macd(symbol)
-        bbd, bbu = TechnicalAnalyser.get_bollinger_band(symbol, timeframe='1h', n=20)
-        logger.info("""
-            symbol: {},
-            prev_cci: {},
-            cci: {},
-            prev_macd: {},
-            macd: {},
-            bbd: {},
-            bbu: {}
-        """.format(symbol, prev_cci, cci, prev_macd, macd, bbd, bbu))
-        is_appropriate = False
-        risk = side = sign = None
-        pbc = PublicClient()
-        ohlc = pbc.fetch_ohlcv(symbol, timeframe='1h', limit=1)[0]
-        close = ohlc[4]
-
-        if prev_cci < -100 < cci and macd > 0 and prev_macd > 0:
-            side = 'buy'
-            sign = 1
-            risk = (close - bbd) / close
-            reward = (bbu - close) / close
-            rr = risk / reward
-            logger.info('rr: {}'.format(rr))
-            if 0 < rr < 1 / 3:
-                is_appropriate = True
-                appropriate_longs.append(symbol)
-        elif prev_cci > 100 > cci and macd < 0 and prev_macd < 0:
-            side = 'sell'
-            sign = -1
-            risk = (bbu - close) / close
-            reward = (close - bbd) / close
-            rr = risk / reward
-            logger.info('rr: {}'.format(rr))
-            if 0 < rr < 1 / 3:
-                is_appropriate = True
-                appropriate_shorts.append(symbol)
-        if is_appropriate:
-            logger.info("""
-            position is appropriate
-            """)
-            tp1 = close * (1 + sign * risk)
-            tp2 = close * (1 + sign * 2 * risk)
-            stoploss = close * (1 - sign * risk)
-            leverage = min(30 / (100 * risk), 20)
-            signal_data = {'symbol': symbol,
-                           'leverage': leverage,
-                           'steps': [{'entry_price': -1, }],
-                           'targets': [{'tp_price': tp1}, {'tp_price': tp2}],
-                           'side': side,
-                           'stoploss': {'trigger_price': stoploss}}
-            position_data = {
-                'signal': signal_data,
-                'margin': 2,
-                'order_type': 'market'
-            }
-            credential_id = 'kucoin_main'
-            bot_data = {
-                'exchange_id': 'kucoin',
-                'credential_id': credential_id,
-                'strategy': 'manual',
-                'position': position_data,
-            }
-            FuturesBotTrader.create_bot(bot_data)
+        auto_trader_per_symbol(symbol, appropriate_longs, appropriate_shorts)
         sleep(1.25)
 
     message = 'appropriate symbols for long positions:{}\n appropriate symbols for short positions:{}'.format(
         appropriate_longs, appropriate_shorts)
     notify_in_telegram(message)
     gc.collect()
+
+
+@catch_all_exceptions()
+def auto_trader_per_symbol(symbol, appropriate_longs, appropriate_shorts):
+    logger = my_get_logger()
+    cci, prev_cci = TechnicalAnalyser.get_cci(symbol=symbol,
+                                              timeframe='1h',
+                                              n=20)
+    macd, prev_macd = TechnicalAnalyser.get_macd(symbol)
+    bbd, bbu = TechnicalAnalyser.get_bollinger_band(symbol, timeframe='1h', n=20)
+    logger.info("""
+        symbol: {},
+        prev_cci: {},
+        cci: {},
+        prev_macd: {},
+        macd: {},
+        bbd: {},
+        bbu: {}
+    """.format(symbol, prev_cci, cci, prev_macd, macd, bbd, bbu))
+    is_appropriate = False
+    risk = side = sign = None
+    pbc = PublicClient()
+    ohlc = pbc.fetch_ohlcv(symbol, timeframe='1h', limit=1)[0]
+    close = ohlc[4]
+
+    if prev_cci < -100 < cci and macd > 0 and prev_macd > 0:
+        side = 'buy'
+        sign = 1
+        risk = (close - bbd) / close
+        reward = (bbu - close) / close
+        rr = risk / reward
+        logger.info('rr: {}'.format(rr))
+        if 0 < rr < 1 / 3:
+            is_appropriate = True
+            appropriate_longs.append(symbol)
+    elif prev_cci > 100 > cci and macd < 0 and prev_macd < 0:
+        side = 'sell'
+        sign = -1
+        risk = (bbu - close) / close
+        reward = (close - bbd) / close
+        rr = risk / reward
+        logger.info('rr: {}'.format(rr))
+        if 0 < rr < 1 / 3:
+            is_appropriate = True
+            appropriate_shorts.append(symbol)
+    if is_appropriate:
+        logger.info("""
+        position is appropriate
+        """)
+        tp1 = close * (1 + sign * risk)
+        tp2 = close * (1 + sign * 2 * risk)
+        stoploss = close * (1 - sign * risk)
+        leverage = min(30 / (100 * risk), 20)
+        signal_data = {'symbol': symbol,
+                       'leverage': leverage,
+                       'steps': [{'entry_price': -1, }],
+                       'targets': [{'tp_price': tp1}, {'tp_price': tp2}],
+                       'side': side,
+                       'stoploss': {'trigger_price': stoploss}}
+        position_data = {
+            'signal': signal_data,
+            'margin': 2,
+            'order_type': 'market'
+        }
+        credential_id = 'kucoin_main'
+        bot_data = {
+            'exchange_id': 'kucoin',
+            'credential_id': credential_id,
+            'strategy': 'manual',
+            'position': position_data,
+        }
+        FuturesBotTrader.create_bot(bot_data)
