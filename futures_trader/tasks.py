dbd17d26f0a5fec0be4c8570575b7de1b28df6a0
Mojahed <mohamad97mj@gmail.com>
2022-10-11 14:32:00 +0330

    bb to 1/3

--- futures_trader/tasks.py
+++ futures_trader/tasks.py
@@ -52,11 +52,11 @@ def auto_trader_per_symbol(symbol):
         confirmations.append('CCI')
         side = 'buy'
         risk = (close - bbd) / close
-        stoploss = bbd
-        tp1 = tp2 = close * (1 + 2 * risk)
+        # stoploss = bbd
+        # tp1 = tp2 = close * (1 + 2 * risk)
         reward = (bbu - close) / close
         rr = risk / reward
-        if 0 < rr < 1 / 3:
+        if 0 < rr < 1 / 2:
             confirmations.append('Bollinger Bands')
         if macd > 0 and prev_macd > 0:
             confirmations.append('Trend')
@@ -64,39 +64,38 @@ def auto_trader_per_symbol(symbol):
     elif prev_cci > 100 > cci:
         confirmations.append('CCI')
         side = 'sell'
-        stoploss = bbu
         risk = (bbu - close) / close
-        tp1 = tp2 = close * (1 - 2 * risk)
+        # stoploss = bbu
+        # tp1 = tp2 = close * (1 - 2 * risk)
         reward = (close - bbd) / close
         rr = risk / reward
-        if 0 < rr < 1 / 3:
+        if 0 < rr < 1 / 2:
             confirmations.append('Bollinger Bands')
         if macd < 0 and prev_macd < 0:
             confirmations.append('Trend')
     if len(confirmations) == 3:
-        leverage = min(int(10 / (100 * risk)), 20)
+        # leverage = min(int(10 / (100 * risk)), 20)
         signal_data = {'symbol': symbol,
                        'side': side,
-                       'confirmations': confirmations,
-                       'leverage': leverage}
+                       'confirmations': confirmations}
         FuturesSignal.objects.create(**signal_data)
-        signal_data = {'obj': signal,
-                       'steps': [{'entry_price': -1, }],
-                       'targets': [{'tp_price': tp1}, {'tp_price': tp2}],
-                       'stoploss': {'trigger_price': stoploss}}
-        position_data = {
-            'signal': signal_data,
-            'margin': 25,
-        }
-        bot_data = {
-            'exchange_id': 'kucoin',
-            'credential_id': 'kucoin_main',
-            'strategy': 'manual',
-            'position': position_data,
-        }
-        FuturesBotTrader.create_bot(bot_data)
-        signal.status = 'confirmed'
-        signal.save()
+        # signal_data = {'obj': signal,
+        #                'steps': [{'entry_price': -1, }],
+        #                'targets': [{'tp_price': tp1}, {'tp_price': tp2}],
+        #                'stoploss': {'trigger_price': stoploss}}
+        # position_data = {
+        #     'signal': signal_data,
+        #     'margin': 25,
+        # }
+        # bot_data = {
+        #     'exchange_id': 'kucoin',
+        #     'credential_id': 'kucoin_main',
+        #     'strategy': 'manual',
+        #     'position': position_data,
+        # }
+        # FuturesBotTrader.create_bot(bot_data)
+        # signal.status = 'confirmed'
+        # signal.save()
         return symbol
 
 
--- process.py
+++ process.py
@@ -5,28 +5,45 @@ from dataclasses import dataclass
 from spot_trader.services.ta import TechnicalAnalyser
 from global_utils import utc2local
 from pprint import pprint
+from spot_trader.clients import PublicClient
 
 
-def one2four(arr):
+def one2four(arr, last_datetime: datetime):
+    hour = last_datetime.hour
+    r = hour % 4
+    first_repeat = 4 - r
+    last_repeat = r
     result = []
-    for i in arr:
+    for j in range(first_repeat):
+        result.append(arr[0])
+    for i in arr[1:-1]:
         for j in range(4):
             result.append(i)
+    for j in range(last_repeat):
+        result.append(arr[-1])
     return result
 
 
 def read_strategy_data():
     records = {}
-
+    pb = PublicClient()
     for symbol in symbols:
-        f = open("data/ohlcv/{}.txt".format(symbol.replace('/', '')), "r")
-        ohlcvs = f.readline()
-        ohlcv_list = literal_eval(ohlcvs)
-        ccis = TechnicalAnalyser.get_ccis(symbol=symbol, timeframe='1h', n=20, ohlcvs=ohlcv_list)[:-1]
-        bb = one2four(TechnicalAnalyser.get_bollinger_bands(symbol=symbol, timeframe='4h', n=20, limit=180))[:-1]
-        macd = one2four(TechnicalAnalyser.get_macds(symbol=symbol, timeframe='4h', limit=180))[:-1]
+        # f = open("data/ohlcv/{}.txt".format(symbol.replace('/', '')), "r")
+        # ohlcvs = f.readline()
+        ohlcv_list = pb.fetch_ohlcv(
+            symbol=symbol,
+            timeframe='1h',
+            limit=1000,
+        )
         ohlcvs_filtered = [[datetime.fromtimestamp(int(ohlcv[0]) / 1000), ohlcv[2], ohlcv[3], ohlcv[4]]
                            for ohlcv in ohlcv_list[:-1]]
+        ccis = TechnicalAnalyser.get_ccis(symbol=symbol, timeframe='1h', n=20, ohlcvs=ohlcv_list)[:-1]
+        bb = one2four(TechnicalAnalyser.get_bollinger_bands(symbol=symbol, timeframe='4h', n=20, limit=252)[:-1],
+                      ohlcvs_filtered[-1][0])[:-1]
+
+        # bb = TechnicalAnalyser.get_bollinger_bands(symbol=symbol, timeframe='1h', n=20, limit=1000)[:-1]
+        macd = one2four(TechnicalAnalyser.get_macds(symbol=symbol, timeframe='4h', limit=252)[:-1],
+                        ohlcvs_filtered[-1][0])[:-1]
         for i in range(len(ohlcvs_filtered)):
             ohlcvs_filtered[i].append(ccis[i])
             ohlcvs_filtered[i].append(bb[i][0])
@@ -59,7 +76,7 @@ def run():
     for symbol in symbols:
         positions[symbol] = []
         open_position = None
-        for i in range(20, len(records[symbol])):
+        for i in range(179, len(records[symbol])):
             record = records[symbol][i]
             date = record[0]
             high = record[1]
@@ -70,6 +87,8 @@ def run():
             bb_u = record[6]
             macd = record[7]
             prv_cci = records[symbol][i - 1][4]
+            if not (bb_d and macd and prv_cci):
+                continue
             # prv_macd = records[symbol][i - 1][7]
             is_long = prv_cci < -100 < cci and macd > 0
             is_short = prv_cci > 100 > cci and macd < 0
@@ -131,12 +150,12 @@ def run():
                         risk = (bb_u - close) / close
                         reward = (close - bb_d) / close
                         # is_consolidation = 100 * (bb_u - bb_d) / bb_u < 5
-                    tp1 = close * (1 + 2 * sign * risk)
+                    tp1 = close * (1 + 1 * sign * risk)
                     tp2 = close * (1 + 2 * sign * risk)
                     stoploss = close * (1 - sign * risk)
                     rr = risk / reward
-                    if 0 < rr < 1 / 3:
-                    # if True:
+                    if 0 < rr < 1 / 2:
+                        # if True:
                         open_position = Position(opened_at=date,
                                                  macd=macd,
                                                  direction=direction,
