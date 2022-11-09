32128499c6f14ed766929950666baee782d8aecb
Mojahed <mohamad97mj@gmail.com>
2022-11-09 16:54:45 +0330

    gc collect was removed

--- forex_trader/tasks.py
+++ forex_trader/tasks.py
@@ -242,8 +242,6 @@ def generate_technical_signals():
 
     message = 'appropriate symbols:{}\n'.format(appropriate_symbols)
     notify_in_telegram(message, 'My alarms')
-    gc.collect()
-    time.sleep(1)
 
 
 @catch_all_exceptions()
